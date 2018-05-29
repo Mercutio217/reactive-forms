aa2f7671601ead40c03a8e130a1ecc0a64f9cd61
Łukasz Bielenin <l.bielenin@gmail.com>
2018-05-29 23:21:53 +0200

    Bind error messages to controls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent implements OnInit {
     this.signupForm = new FormGroup(
       {
         'userData': new FormGroup({
-          'username': new FormControl(null, Validators.required),
+          'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
           'email': new FormControl(null, [Validators.required, Validators.email])
         })
 ,
@@ -37,8 +37,8 @@ export class AppComponent implements OnInit {
     (<FormArray> this.signupForm.get('hobbies')).push(control);
   }
 
-  forbiddenNames(control: FormControl): {[s: string] :  boolean } {
-    if (this.forbiddenUsernames.indexOf(control.value)) {
+  forbiddenNames(control: FormControl): {[s: string]:  boolean } {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1 ) {
       return {'name is forbidden' : true};
     }
       return null;
